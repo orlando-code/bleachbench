00c85b2e292337cd0fd8f13986eecdf877990379
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-11 10:57:20 +0100

    added simple plot formatting

---
+++ src/utils/plotting.py
@@ -0,0 +1,19 @@
+# general
+# spatial
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+
+
+def format_geo_axes(
+    ax: plt.Axes, extent: tuple | list = (-180, 180, -40, 50)
+) -> plt.Axes:
+    ax.set_extent(extent, crs=ccrs.PlateCarree())
+    ax.add_feature(cfeature.LAND, facecolor="white")
+    ax.add_feature(cfeature.OCEAN, alpha=0.3)
+    ax.add_feature(cfeature.COASTLINE, edgecolor="lightgray", zorder=-1)
+    ax.add_feature(
+        cfeature.BORDERS, linestyle=":", edgecolor="gray", alpha=0.1, zorder=-1
+    )
+
+    return ax
