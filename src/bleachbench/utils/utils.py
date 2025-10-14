e1f07fd83c4f29ba4ee1af06454d5be3cc217398
Orlando Timmerman <rt582@cam.ac.uk>
2025-10-14 21:38:59 +0000

    add climatology regression functions

--- src/bleachbench/utils/utils.py
+++ src/bleachbench/utils/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import xarray as xa
 
 
 def parse_date(date):
@@ -23,6 +24,40 @@ def parse_date(date):
     raise ValueError(f"Invalid date format: {date}")
 
 
-def get_bounds(ds, var_key):
+def get_bounds(ds: xa.Dataset, var_key: str) -> tuple[float, float]:
+    """Get the minimum and maximum values of a variable in a dataset."""
     min_val, max_val = ds[var_key].values.min(), ds[var_key].values.max()
     return min_val, max_val
+
+
+def regress_climatology(clim_for_month: xa.DataArray) -> tuple[xa.DataArray, xa.DataArray]:
+    """
+    Accepting a dataarray for a single month, calculate the linear regression
+    coefficients (slope and intercept) of the monthly average temperatures over 
+    the 28-year period (1985-2012)
+    
+    Args:
+        clim_for_month (xa.DataArray): Dataarray for a single month
+
+    Returns:
+        Tuple of slope (m) and intercept (c), so one can compute y = m*x + c.
+    """
+    x = clim_for_month.time.dt.year.values
+    y = clim_for_month
+    # calculate values for broadcasting
+    x_mean = x.mean()
+    y_mean = y.mean(dim='time')
+    x_da = xa.DataArray(x, dims=['time'], coords={'time': clim_for_month['time']})
+    cov_xy = ((x_da - x_mean) * (y - y_mean)).mean(dim='time')
+    var_x = ((x_da - x_mean) ** 2).mean(dim='time')
+    slope = cov_xy / var_x
+    intercept = y_mean - slope * x_mean
+    return slope, intercept
+
+def predict_climatology(slope: xa.DataArray, intercept: xa.DataArray, x_target: float) -> xa.DataArray:
+    """
+    Predict climatology at a given x_target (year) given linear slope and intercept.
+    y = slope * x_target + intercept
+    """
+    return intercept + slope * x_target
+        
